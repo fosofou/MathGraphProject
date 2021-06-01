e0cc2f79bb7a8958c5e1c1d7f5037a4b557e43d3
fosofou <57597439+fosofou@users.noreply.github.com>
2021-06-01 20:42:56 +0500

    

--- Front/static/js/mobile.js
+++ Front/static/js/mobile.js
@@ -3,6 +3,6 @@ var tograph = document.querySelector('.border-button');
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 
-    tograph.setAttribute('href', "mobile");
+    tograph.setAttribute('href', '{% url "mobile" %}');
 }
 }
